be77a752cb3e962eefe92d7873ead154aa57b669
Miguelette79 <62026419+Miguelette79@users.noreply.github.com>
2020-03-12 16:20:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,17 +15,17 @@ color_codes = {
 }
 color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue","blue2","darkblue","pink"])
 @app.route("/")
-def main():
-#return 'Hello'
-print(color)
-return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
+  def main():
+  #return 'Hello'
+  print(color)
+  return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
 @app.route('/color/<new_color>')
-def new_color(new_color):
-return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
+  def new_color(new_color):
+  return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
 @app.route('/read_file')
-def read_file():
-f = open("/data/testfile.txt")
-contents = f.read()
-return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+  def read_file():
+  f = open("/data/testfile.txt")
+  contents = f.read()
+  return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
 if __name__ == "__main__":
 app.run(host="0.0.0.0", port="8080")
